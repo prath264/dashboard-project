b677641fa8e26554db1775d8c3ea51490ba91b33
Prath <prath264@gmail.com>
2026-08-05 20:51:39 +0530

    updates links

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router';
 import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
 
 import './App.css'
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="checkout" element={<div>test</div>}></Route>
+      <Route path="checkout" element={<CheckoutPage />}></Route>
 
     </Routes>
     
